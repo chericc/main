36a94e7802d2885b36b4cfb012c3a6ea6f72ab00
favccc <1461021702@qq.com>
2023-05-09 14:48:16 +0000

    u: wav_demuxer / fin

--- audio/wav/demuxer/wav_demuxer.cpp
+++ audio/wav/demuxer/wav_demuxer.cpp
@@ -230,21 +230,24 @@ int WavDemuxer::readHeader(std::shared_ptr<LoadInfo> info)
                 xlog_trc("parsing chunk fmt");
                 info->fmt.tag.chunkid = chunkid;
                 info->fmt.tag.chunksize = chunksize;
+                std::size_t next_tag_pos = info->xio->tell() + chunksize;
                 info->fmt.audio_format = info->xio->rl16();
                 info->fmt.num_channels = info->xio->rl16();
                 info->fmt.sample_rate = info->xio->rl32();
                 info->fmt.byte_rate = info->xio->rl32();
                 info->fmt.align = info->xio->rl16();
                 info->fmt.bits_per_sample = info->xio->rl16();
+                if (info->xio->tell() != next_tag_pos)
+                {
+                    xlog_trc("additional info in fmt, skip");
+                    if (info->xio->seek(next_tag_pos, SEEK_SET) < 0)
+                    {
+                        xlog_err("seek to next tag failed");
+                        berror = true;
+                        break;
+                    }
+                }
                 has_fmt = true;
-                xlog_trc("fmt=%d,num_chn=%d,sample_rate=%d,"
-                    "byte_rate=%d,align=%d,bits_per_sample=%d",
-                    (int)info->fmt.audio_format,
-                    (int)info->fmt.num_channels,
-                    (int)info->fmt.sample_rate,
-                    (int)info->fmt.byte_rate,
-                    (int)info->fmt.align,
-                    (int)info->fmt.bits_per_sample);
                 break;
             }
             case MKTAG('d', 'a', 't', 'a'):
@@ -304,140 +307,3 @@ int WavDemuxer::readHeader(std::shared_ptr<LoadInfo> info)
     }
     return 0;
 }
-
-#if 0
-int WavDemuxer::readHeader(std::shared_ptr<LoadInfo> info)
-{
-    int berror = false;
-
-    size_t offset = 0;  // 下一个要扫描的块的起始位置
-    size_t size = 0;    // 临时读大小
-
-    bool has_riff = false;
-    bool has_fmt = false;
-    bool has_data = false;
-
-    if (!info)
-    {
-        xlog_err("null info");
-        return -1;
-    }
-
-    for (;;)
-    {
-        if (berror)
-        {
-            break;
-        }
-
-        xlog_dbg("offset=%lu", offset);
-
-        // get next tag
-        size = sizeof(NA_TAG);
-        std::vector<uint8_t> buftag = readBuf(info, offset, size);
-        if (!buftag.data() || buftag.size() != size)
-        {
-            xlog_dbg("no next tag");
-            break;
-        }
-
-        NA_TAG tag{};
-        memcpy(&tag, buftag.data(), size);
-
-        std::vector<uint8_t> buf;
-        switch (tag.chunkid)
-        {
-            case MKTAG('R', 'I', 'F', 'F'):
-            {
-                xlog_dbg("parsing chunk RIFF");
-                size = sizeof(NA_RIFF);
-                buf = readBuf(info, offset, size);
-                NA_RIFF *nariff = (NA_RIFF*)buf.data();
-                if (!nariff || buf.size() != size)
-                {
-                    xlog_err("parse chunk riff failed");
-                    berror = true;
-                    break;
-                }
-                
-                memcpy(&info->riff, nariff, size);
-                offset += size;
-
-                has_riff = true;
-
-                break;
-            }
-            case MKTAG('f','m','t',' '):
-            {
-                xlog_dbg("parsing chunk fmt");
-                size = sizeof(NA_SubChunkFmt);
-                buf = readBuf(info, offset, size);
-                NA_SubChunkFmt* nafmt = (NA_SubChunkFmt*)buf.data();
-                if (!nafmt || buf.size() != size)
-                {
-                    xlog_err("parse chunk fmt failed");
-                    berror = true;
-                    break;
-                }
-
-                memcpy(&info->fmt, nafmt, size);
-                offset += tag.chunksize + sizeof(NA_TAG);
-
-                has_fmt = true;
-
-                break;
-            }
-            case MKTAG('d', 'a', 't', 'a'):
-            {
-                xlog_dbg("parsing chunk data");
-                size = sizeof(NA_SubChunkData);
-                buf = readBuf(info, offset, size);
-                NA_SubChunkData* data = (NA_SubChunkData*)buf.data();
-                if(!data || buf.size() != size)
-                {
-                    xlog_err("parse chunk data failed");
-                    berror = true;
-                    break;
-                }
-
-                info->data_offset = offset + sizeof(NA_TAG);
-                info->data_size = data->tag.chunksize;
-                offset += tag.chunksize + sizeof(NA_TAG);
-
-                has_data = true;
-
-                break;
-            }
-            default:
-            {
-                xlog_dbg("parsing chunk unknown");
-                offset += tag.chunksize + sizeof(NA_TAG);
-                break;
-            }
-        } // switch
-    } // for()
-
-    if (!has_riff || !has_fmt || !has_data)
-    {
-        xlog_err("chunk incomplete(riff=%d,fmt=%d,data=%d)", 
-            has_riff, has_fmt, has_data);
-        berror = true;
-    }
-
-    // 为了读取的方便，做出一些限制
-    if ((info->fmt.bits_per_sample % 8 != 0)
-        || info->fmt.num_channels != 1)
-    {
-        xlog_err("bits_per_sample=%d, num_channels=%d", 
-            info->fmt.bits_per_sample,
-            info->fmt.num_channels);
-        berror = true;
-    }
-
-    if (berror)
-    {
-        return -1;
-    }
-    return 0;
-}
-#endif
