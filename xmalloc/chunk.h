d62c8cd6a6a43b4db327f60ae0e26d95f70c3680
favccc <1461021702@qq.com>
2023-06-15 19:18:18 +0800

    u: remove xmalloc ( not a good idea )

--- xmalloc/chunk.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int xmalloc_chunk_startup();
-
-void *xmalloc_chunk_malloc(unsigned long size);
-int xmalloc_chunk_free(void *ptr);
-void *xmalloc_chunk_realloc(void *oldptr, unsigned long new_size);
--- xmalloc/chunk_def.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct skip_alloc_st 
-{
-    unsigned char sa_flags;
-
-    unsigned int sa_user_space;
-    unsigned int sa_total_size;
-} skip_alloc_t;
