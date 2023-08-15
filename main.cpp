ae6b07754f9decd9a4be261b526481e59c125294
chericc <1461021702@qq.com>
2023-08-15 13:58:33 +0800

    u: test/err logs only

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
 	std::vector<FILE*> fps = { stdout };
 	xlog_setoutput(fps);
-	// xlog_setmask(XLOG_MASK_ERR);
+	xlog_setmask(XLOG_MASK_ERR);
 
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
