a591361d8f12b65e682534a047c5bd9583d7b9e4
chericc <1461021702@qq.com>
2023-11-16 22:32:37 +0800

    u: pcap2ts printinfo

--- xmedia/pcap2ts/demo/demo_pcap2ts.cpp
+++ xmedia/pcap2ts/demo/demo_pcap2ts.cpp
@@ -7,11 +7,11 @@
 #include "xlog.hpp"
 #include "xutility.hpp"
 
-static std::pair<std::string,std::string> ipv4addr(const std::array<uint8_t, 8> &ipv4)
+static std::pair<std::string,std::string> str_ipv4addr(const std::array<uint8_t, 8> &ipv4)
 {
     std::string ipstr;
     std::string maskstr;
-    char str_tmp[64];
+    char str_tmp[256];
 
     snprintf(str_tmp, sizeof(str_tmp), "%" PRIu8 ".%" PRIu8 ".%" PRIu8 ".%" PRIu8, 
         ipv4[0], ipv4[1], ipv4[2], ipv4[3]);
@@ -24,17 +24,56 @@ static std::pair<std::string,std::string> ipv4addr(const std::array<uint8_t, 8> 
     return std::make_pair(ipstr, maskstr);
 }
 
-std::string ipv6addr(const std::array<uint8_t, 17> &ipv6)
+static std::string str_ipv6addr(const std::array<uint8_t, 17> &ipv6)
 {
     char str_tmp[256];
     snprintf(str_tmp, sizeof(str_tmp), 
-        "%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 
-        ".%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 ".%" PRIu8 "%" PRIu8 
+        "%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 
+        ".%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 ".%02" PRIx8 "%02" PRIx8 
         "/%" PRIu8,
         ipv6[0], ipv6[1], ipv6[2], ipv6[3], ipv6[4], ipv6[5], ipv6[6], ipv6[7], 
         ipv6[8], ipv6[9], ipv6[10], ipv6[11], ipv6[12], ipv6[13], ipv6[14], ipv6[15], 
         ipv6[16]);
     return std::string(str_tmp);
+}
+
+static std::string str_macaddr(const std::array<uint8_t, 6> &mac)
+{
+    char str_tmp[256];
+    snprintf(str_tmp, sizeof(str_tmp),
+        "%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8,
+        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return std::string(str_tmp);
+}
+
+static std::string str_euiaddr(const std::array<uint8_t, 8> &euiaddr)
+{
+    char str_tmp[256];
+    snprintf(str_tmp, sizeof(str_tmp),
+        "%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8 ".%02" PRIx8,
+        euiaddr[0], euiaddr[1], euiaddr[2], euiaddr[3], 
+        euiaddr[4], euiaddr[5], euiaddr[6], euiaddr[7]);
+    return std::string(str_tmp);
+}
+
+static std::string str_bps(double bps)
+{
+    std::array<std::string, 4> speed_name = {"bps", "kbps", "Mbps", "Gbps"};
+
+    double bitrate = bps;
+    std::size_t i_level = 0;
+    char str[64];
+    for (i_level = 0; i_level < speed_name.size(); ++i_level)
+    {
+        if (bitrate < 1000.0)
+        {
+            break;
+        }
+        bitrate = bitrate / 1000;
+    }
+    
+    snprintf(str, sizeof(str), "%.1g %s", bitrate, speed_name[i_level].c_str());
+    return std::string(str);
 }
 
 static void deal_pcapngcontent(const PcapngContent &content)
@@ -59,16 +98,27 @@ static void deal_pcapngcontent(const PcapngContent &content)
             auto const& intf = content.interface;
             xlog_inf("if_name: %s", intf.if_name.c_str());
             xlog_inf("if_description: %s", intf.if_description.c_str());
+            xlog_inf("ipv4: ");
             for (auto const& it : intf.if_IPv4addrs)
             {
-                auto pair_ipv4 = ipv4addr(it);
+                auto pair_ipv4 = str_ipv4addr(it);
                 xlog_inf("ipv4/mask: %s/%s", pair_ipv4.first.c_str(), pair_ipv4.second.c_str());
             }
+            xlog_inf("ipv6: ");
             for (auto const& it : intf.if_IPV6addrs)
             {
-                auto ipv6str = ipv6addr(it);
+                auto ipv6str = str_ipv6addr(it);
                 xlog_inf("ipv6/prefix: %s", ipv6str.c_str());
             }
+            xlog_inf("mac: %s", str_macaddr(intf.if_MACaddr).c_str());
+            xlog_inf("eui: %s", str_euiaddr(intf.if_EUIaddr).c_str());
+            xlog_inf("speed: %s", str_bps(intf.if_speed).c_str());
+            xlog_inf("os: %s", intf.if_os.c_str());
+            xlog_inf("hardware: %s", intf.if_hardware.c_str());
+            xlog_inf("tx: %s", str_bps(intf.if_txspeed).c_str());
+            xlog_inf("rx: %s", str_bps(intf.if_rxspeed).c_str());
+
+            content_stack.push(content);
         }
     }
     while (0);
