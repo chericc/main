b4cfb453b7e8921791b56ed612830e4afa11a27d
chericc <1461021702@qq.com>
2023-11-27 21:22:31 +0800

    u: pcap2ts: remove unused codes

--- xmedia/pcap2ts/packet_process.cpp
+++ xmedia/pcap2ts/packet_process.cpp
@@ -47,37 +47,6 @@ static void debug_packet(std::shared_ptr<SharedPacket> packet)
             }
         }
     }
-#if 0
-    if (ipacket->type() == PacketType::Ethernet)
-    {
-        std::shared_ptr<EthernetPacket> ep = std::static_pointer_cast<EthernetPacket>(ipacket);
-        xlog_dbg("Ethernet.dst_mac: %s", str_macaddr(ep->mac_dst).c_str());
-        xlog_dbg("Ethernet.src_mac: %s", str_macaddr(ep->mac_src).c_str());
-        xlog_dbg("Ethernet.sub_type: %s", ep->subTypeName(ep->convertEthType(ep->type)));
-        xlog_dbg("Ethernet.sub_size: %zu", ep->data().offsetSize());
-    }
-    else if(ipacket->type() == PacketType::IPv4)
-    {
-        std::shared_ptr<IPv4EthernetPacket> ipv4p = std::static_pointer_cast<IPv4EthernetPacket>(ipacket);
-        xlog_dbg("ipv4->version: %" PRIu8, ipv4p->ipv4().version);
-        xlog_dbg("ipv4->len: %" PRIu8, ipv4p->ipv4().len);
-        xlog_dbg("ipv4->differentialted_service_field: %" PRIu8, ipv4p->ipv4().differentialted_services_field);
-        xlog_dbg("ipv4->total_length: %" PRIu16, ipv4p->ipv4().total_length);
-        xlog_dbg("ipv4->identification: %#" PRIx16, ipv4p->ipv4().identification);
-        xlog_dbg("ipv4->flag: %" PRIu16, ipv4p->ipv4().flag);
-        xlog_dbg("ipv4->fragment_offset: %" PRIu16, ipv4p->ipv4().fragment_offset);
-        xlog_dbg("ipv4->time_to_live: %" PRIu8, ipv4p->ipv4().time_to_live);
-        xlog_dbg("ipv4->protocol: %" PRIu8, ipv4p->ipv4().protocol);
-        xlog_dbg("ipv4->header_checksum: %#" PRIx16, ipv4p->ipv4().header_checksum);
-        xlog_dbg("ipv4->ip_src: %s", str_ipv4(ipv4p->ipv4().ip_addr_src).c_str());
-        xlog_dbg("ipv4->ip_dst: %s", str_ipv4(ipv4p->ipv4().ip_addr_dst).c_str());
-        xlog_dbg("ipv4->padding_size: %zu", ipv4p->ipv4().padding_size);
-    }
-    else 
-    {
-        xlog_dbg("Type not support(%d)", (int)ipacket->type());
-    }
-#endif 
 }
 
 PacketProcess::PacketProcess()
