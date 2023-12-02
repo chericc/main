5840bc9044463b9af37a0c767315235b0679c129
chericc <1461021702@qq.com>
2023-12-02 08:54:15 +0800

    u: seek & tell supports macos

--- xtools/xcrossplatform/xos_independent.cpp
+++ xtools/xcrossplatform/xos_independent.cpp
@@ -6,6 +6,9 @@ int x_fseek64(FILE *stream, int64_t offset, int whence)
 
 #if defined(X_PLATFORM_MSVC)
     ret = _fseeki64(stream, offset, whence);
+#elif defined(X_PLATFORM_CLANG)
+    static_assert(sizeof(off_t) == sizeof(int64_t), "");
+    ret = fseeko(stream, offset, whence);
 #else 
     ret = fseeko64(stream, offset, whence);
 #endif
@@ -19,6 +22,9 @@ int64_t x_ftell64(FILE *stream)
 
 #if defined(X_PLATFORM_MSVC)
     ret = _ftelli64(stream);
+#elif defined(X_PLATFORM_CLANG)
+    static_assert(sizeof(off_t) == sizeof(int64_t), "");
+    ret = ftello(stream);
 #else 
     ret = ftello64(stream);
 #endif 
