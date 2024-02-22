8f4e3fb92aec981d4e01c5b2ea5b22ca6606a70d
chericc <1461021702@qq.com>
2024-02-22 15:04:18 +0000

    u: crc_demo tmp upload

--- xtools/xutility/demo_crc/crc.cpp
+++ xtools/xutility/demo_crc/crc.cpp
@@ -1,4 +1,39 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+
 #include "xlog.hpp"
+#include "xcrc.hpp"
+
+static std::string crc_filestream(FILE *fp)
+{
+    
+}
+
+static std::string crc_file(const std::string file)
+{
+    FILE *fp = nullptr;
+
+    do 
+    {
+        if (file.empty())
+        {
+            xlog_err("Filename empty");
+            break;
+        }
+
+        fp = fopen(file.c_str(), "rb");
+        if (!fp)
+        {
+            char err_buf[64];
+            int error_num = errno;
+            strerror_r(error_num, err_buf, sizeof(err_buf));
+            xlog_err("Open file failed(%s,%s)", file.c_str(), err_buf);
+            break;
+        }
+    }
+    while (0);
+}
 
 int main()
 {
@@ -6,6 +41,8 @@ int main()
     xlog_setoutput(output);
 
     xlog_dbg("hello world");
+
+
 
     return 0;
 }
