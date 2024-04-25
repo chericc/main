0b5358968893f6f4ba30de0ab026f791a317e231
chericc <1461021702@qq.com>
2024-04-25 17:26:00 +0800

    u: httpserver + get support, not fin(path not right)

--- demos/network/libevent/httpserver/httpserver.cpp
+++ demos/network/libevent/httpserver/httpserver.cpp
@@ -1,7 +1,13 @@
 #include <array>
-
+#include <cstring>
 #include <stdarg.h>
+
 #include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
 
 #include <event2/event.h>
 #include <event2/http.h>
@@ -9,6 +15,7 @@
 #include <event2/buffer.h>
 
 #include "xlog.hpp"
+#include "xos_independent.hpp"
 
 struct options
 {
@@ -16,7 +23,25 @@ struct options
     const char *docroot = nullptr;
 };
 
-static void print_usage(FILE *out, int argc, char **argv)
+static const struct table_entry {
+	const char *extension;
+	const char *content_type;
+} content_type_table[] = {
+	{ "txt", "text/plain" },
+	{ "c", "text/plain" },
+	{ "h", "text/plain" },
+	{ "html", "text/html" },
+	{ "htm", "text/htm" },
+	{ "css", "text/css" },
+	{ "gif", "image/gif" },
+	{ "jpg", "image/jpeg" },
+	{ "jpeg", "image/jpeg" },
+	{ "png", "image/png" },
+	{ "pdf", "application/pdf" },
+	{ "ps", "application/postscript" }
+};
+
+static void print_usage(FILE *out, int , char **argv)
 {
     fprintf(out, 
         "Usage: %s [ OPTS ] <docroot>\n"
@@ -61,10 +86,46 @@ static bool signal_ignore()
 
 static void on_term(int sig, short events, void *arg)
 {
-    xlog_dbg("interrupted, terminating");
+    xlog_dbg("interrupted(sig=%d, events=%#hx), terminating", sig, events);
     auto base = reinterpret_cast<event_base*>(arg);
     event_base_loopbreak(base);
     return ;
+}
+
+static const char *guess_content_type(const char *path)
+{
+    bool found = false;
+    const char *content_type = nullptr;
+
+    do 
+    {
+        const char *last = nullptr;
+        const char *extension = nullptr;
+        last = strrchr(path, '.');
+
+        if (!last)
+        {
+            break;
+        }
+
+        extension = last + 1;
+        for (auto const &ref : content_type_table)
+        {
+            if (!strcasecmp(ref.extension, extension))
+            {
+                content_type = ref.content_type;
+                found = true;
+                break;
+            }
+        }
+    }
+    while (false);
+    
+    if (!found)
+    {
+        content_type = "application/octet-stream";
+    }
+    return content_type;
 }
 
 static const char *http_req_type_str(enum evhttp_cmd_type type)
@@ -153,7 +214,7 @@ static std::string dump_req_content(struct evhttp_request *req)
     return str;
 }
 
-static void dump_request_cb(struct evhttp_request *req, void *arg)
+static void dump_request_cb(struct evhttp_request *req, void *)
 {
     auto req_type = evhttp_request_get_command(req);
     const char *req_type_str = http_req_type_str(req_type);
@@ -166,9 +227,252 @@ static void dump_request_cb(struct evhttp_request *req, void *arg)
     evhttp_send_reply(req, 200, "OK", nullptr);
 }
 
+static int generate_html_of_path(evbuffer *evbuf, const char *path_short, const char *path_real, int *error_code)
+{
+    bool error = true;
+    DIR *dir = nullptr;
+    int fd = -1;
+
+    do
+    {
+        if (!evbuf || !path_short || !path_real || !error_code)
+        {
+            xlog_err("null");
+            break;
+        }
+
+        xlog_dbg("path:<%s, %s>", path_short, path_real);
+
+        struct stat st = {};
+        if (stat(path_real, &st) < 0)
+        {
+            int errno_local = errno;
+            std::array<char, 64> buf_str = {};
+            x_strerror(errno_local, buf_str.data(), buf_str.size());
+            xlog_err("stat failed, path(%s): %s", path_real, buf_str.data());
+            *error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        if (S_ISDIR(st.st_mode))
+        {
+            dir = opendir(path_real);
+            if (!dir)
+            {
+                xlog_err("opendir failed");
+                *error_code = HTTP_NOTFOUND;
+                break;
+            }
+            evbuffer_add_printf(evbuf,
+R"(
+<html>
+ <head>
+   <title>Index of %s</title>
+   <base href='%s%s'>
+ </head>
+ <body>
+  <h1>Index of %s</h1>
+  <ul>)"
+            , path_short, path_real, "/", path_short);
+
+            struct dirent *ent = nullptr;
+            while ((ent = readdir(dir)))
+            {
+                const char *name = ent->d_name;
+                // std::string item_path = std::string() + path + "/" + name;
+                std::string item_path = std::string() + name;
+                evbuffer_add_printf(evbuf,
+R"(
+   <li><a href="%s">%s</a>
+)"              , item_path.c_str(), name);
+            }
+
+            evbuffer_add_printf(evbuf, "</ul></body></html>\n");
+        }
+        else 
+        {
+            if ((fd = open(path_real, O_RDONLY)) < 0)
+            {
+                xlog_err("open file failed");
+                break;
+            }
+
+            struct stat st_local = {};
+            if (fstat(fd, &st_local) < 0)
+            {
+                xlog_err("fstat failed");
+                break;
+            }
+
+            evbuffer_add_file(evbuf, fd, 0, st_local.st_size);
+        }
+
+        error = false;
+    }
+    while (0);
+
+    if (dir)
+    {
+        closedir(dir);
+        dir = nullptr;
+    }
+
+    if (fd >= 0)
+    {
+        close(fd);
+        fd = -1;
+    }
+
+    return error ? -1 : 0;
+}
+
 static void send_document_cb(struct evhttp_request *req, void *arg)
 {
     xlog_dbg("in");
+
+    struct evhttp_uri *decoded_uri = nullptr;
+    struct evbuffer *evbuf = nullptr;
+    char *decoded_uri_path = nullptr;
+
+    bool error = true;
+    int http_error_code = 0;
+
+    do 
+    {
+        auto *sopt = (struct options*)arg;
+
+        if (!sopt)
+        {
+            xlog_err("null opt");
+            break;
+        }
+
+        if (evhttp_request_get_command(req) != EVHTTP_REQ_GET)
+        {
+            xlog_err("request command is not GET, dump");
+            dump_req_content(req);
+            break;
+        }
+
+        const char *uri = evhttp_request_get_uri(req);
+        if (!uri)
+        {
+            xlog_err("evhttp_request_get_uri failed");
+            http_error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        xlog_dbg("uri: %s", uri);
+
+        decoded_uri = evhttp_uri_parse(uri);
+        if (!decoded_uri)
+        {
+            xlog_err("evhttp_uri_parse failed");
+            http_error_code = HTTP_BADREQUEST;
+            break;
+        }
+
+        const char *uri_path = evhttp_uri_get_path(decoded_uri);
+        if (!uri_path)
+        {
+            uri_path = "/";
+        }
+        xlog_dbg("uri_path: %s", uri_path);
+
+        decoded_uri_path = evhttp_uridecode(uri_path, 0, nullptr);
+        if (!decoded_uri_path)
+        {
+            http_error_code = HTTP_NOTFOUND;
+            xlog_err("evhttp_uridecode failed");
+            break;
+        }
+        xlog_dbg("decoded_uri_path: %s", decoded_uri_path);
+
+        if (strstr(decoded_uri_path, ".."))
+        {
+            xlog_dbg("path contains \"..\", ignored");
+            http_error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        if (!sopt->docroot)
+        {
+            xlog_err("root is null");
+            http_error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        std::string whole_path = std::string() + sopt->docroot + "/" + decoded_uri_path;
+
+        evbuf = evbuffer_new();
+        if (!evbuf)
+        {
+            xlog_err("evbuffer_new");
+            break;
+        }
+
+        // if (generate_html_of_path(evbuf, whole_path.c_str(), &http_error_code) < 0)
+        if (generate_html_of_path(evbuf, decoded_uri_path, whole_path.c_str(), &http_error_code) < 0)
+        {
+            xlog_err("generate html failed");
+            http_error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        struct stat st = {};
+        if (stat(whole_path.c_str(), &st) < 0)
+        {
+            xlog_err("stat failed");
+            http_error_code = HTTP_NOTFOUND;
+            break;
+        }
+
+        if (S_ISDIR(st.st_mode))
+        {
+            evhttp_add_header(evhttp_request_get_output_headers(req),
+                "Content-Type", "text/html");
+        }
+        else 
+        {
+            const char *type = guess_content_type(whole_path.c_str());
+            evhttp_add_header(evhttp_request_get_output_headers(req),
+                "Content-Type", type);
+        }
+
+        error = false;
+    }
+    while (0);
+
+    if (error)
+    {
+        if (http_error_code != 0)
+        {
+            evhttp_send_error(req, http_error_code, nullptr);
+        }
+    }
+    else 
+    {
+        evhttp_send_reply(req, HTTP_OK, "OK", evbuf);
+    }
+
+    if (decoded_uri_path)
+    {
+        free(decoded_uri_path);
+        decoded_uri_path = nullptr;
+    }
+
+    if (decoded_uri)
+    {
+        evhttp_uri_free(decoded_uri);
+        decoded_uri = nullptr;
+    }
+
+    if (evbuf)
+    {
+        evbuffer_free(evbuf);
+        evbuf = nullptr;
+    }
+
     xlog_dbg("out");
     return ;
 }
@@ -205,7 +509,8 @@ int main(int argc, char **argv)
             break;
         }
 
-        evhttp_set_gencb(http, dump_request_cb, &sopt);
+        evhttp_set_cb(http, "/dump", dump_request_cb, nullptr);
+        evhttp_set_gencb(http, send_document_cb, &sopt);
 
         handle_v4 = evhttp_bind_socket_with_handle(http, "0.0.0.0", sopt.port);
         handle_v6 = evhttp_bind_socket_with_handle(http, "::/0", sopt.port);
