82998ec4942bb5f52d4115084d093ea0e5196949
chericc <1461021702@qq.com>
2024-05-16 14:21:53 +0000

    u: dp/adapter

---
+++ cs/DesignPatterns/Structural/Adapter/text_formatter.cpp
@@ -0,0 +1,37 @@
+#include "text_formatter.hpp"
+
+std::string NewLineFormatter::formatText(std::string const& text)
+{
+    std::string text_copy = text;
+    for (auto &it : text_copy)
+    {
+        if (it == '.')
+        {
+            it = '\n';
+        }
+    }
+    return text_copy;
+}
+
+std::string CsvFormatterImp::formatText(std::string const& text)
+{
+    std::string text_copy = text;
+    for (auto &it : text_copy)
+    {
+        if (it == '.')
+        {
+            it = ',';
+        }
+    }
+    return text_copy;
+}
+
+std::string CsvFormatterAdap::formatText(std::string const& text)
+{
+    if (!csv_formatter)
+    {
+        csv_formatter = std::make_shared<CsvFormatterImp>();
+    }
+
+    return csv_formatter->formatText(text);
+}
---
+++ cs/DesignPatterns/Structural/Adapter/text_formatter.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+class TextFormatter 
+{
+public:
+    virtual ~TextFormatter() = default;
+    virtual std::string formatText(std::string const &text) = 0;
+};
+
+class NewLineFormatter : public TextFormatter
+{
+public:
+    std::string formatText(std::string const& text) override;
+};
+
+class CsvFormatter 
+{
+public:
+    virtual ~CsvFormatter() = default;
+    virtual std::string formatText(std::string const &text) = 0;
+};
+
+class CsvFormatterImp : public CsvFormatter
+{
+public:
+    std::string formatText(std::string const& text) override;
+};
+
+class CsvFormatterAdap : public TextFormatter
+{
+public:
+    std::string formatText(std::string const& text) override;
+private:
+    std::shared_ptr<CsvFormatter> csv_formatter;
+};
