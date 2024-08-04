e5b3f0dca7e30eb87f5949f5152b34f958add59b
chericc <1461021702@qq.com>
2024-08-04 19:45:02 +0800

    +mjson demo

--- demos/mjson/demo_mjson.c
+++ demos/mjson/demo_mjson.c
@@ -4,10 +4,41 @@
 
 static int my_json_snprintf_pretty(char *buf, size_t len, const char *src, size_t len_src)
 {
-    va_list ap;
     struct mjson_fixedbuf fb = {buf, (int) len, 0};
     mjson_pretty(src, len_src, " ", mjson_print_fixed_buf, &fb);
     return fb.len;
+}
+
+/*
+
+cmd:
+
+{
+ "cmd": "test",
+ "a": {
+  "a": 1,
+  "b": "test"
+ },
+ "b": {
+  "number": 0
+ }
+}
+
+
+*/
+
+static const char *type_name(int type)
+{
+    switch (type) {
+        case MJSON_TOK_STRING: return "string";
+        case MJSON_TOK_NUMBER: return "number";
+        case MJSON_TOK_TRUE: return "true";
+        case MJSON_TOK_FALSE: return "false";
+        case MJSON_TOK_NULL: return "null";
+        case MJSON_TOK_ARRAY: return "array";
+        case MJSON_TOK_OBJECT: return "object";
+        default: return "unknown";
+    }
 }
 
 int main()
@@ -29,5 +60,18 @@ int main()
     char cmd_pretty[256] = { 0 };
     my_json_snprintf_pretty(cmd_pretty, sizeof(cmd_pretty), cmd, sizeof(cmd));
     printf("cmd_pretty: \n%s\n", (char*)cmd_pretty);
+
+    if (1)
+    {
+        int koff, klen, voff, vlen, vtype, off;
+
+        for (off = 0; (off = mjson_next(cmd, strlen(cmd), off, &koff, &klen,
+            &voff, &vlen, &vtype)) != 0; )
+        {
+            printf("key: %.*s, value: %.*s, type: %s\n", klen, cmd + koff, vlen, cmd + voff, 
+                type_name(vtype));
+        }
+    }
+
     return 0;
 }
