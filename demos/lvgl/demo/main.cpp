790b604ea4d71d49c6534ef5772d9cb61d7523c6
chericc <1461021702@qq.com>
2025-03-23 17:21:24 +0800

    demo_lvgl: simpe text

--- demos/lvgl/demo/main.cpp
+++ demos/lvgl/demo/main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 
 #include "lvgl/lvgl.h"
+#include "lvgl/src/widgets/label/lv_label.h"
 #include "xlog.hpp"
 
 int main()
@@ -11,6 +12,12 @@ int main()
     lv_init();
 
     lv_display_t *disp = lv_sdl_window_create(800, 600);
+
+    lv_obj_t *screen = lv_screen_active();
+    xlog_dbg("screen: %p\n", screen);
+
+    lv_obj_t *label = lv_label_create(screen);
+    lv_label_set_text(label, "123123");
 
     while (true) {
         uint32_t idle_time = lv_timer_handler();
