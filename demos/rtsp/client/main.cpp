a9f7ba4d4876519960d0c4a72ef827f01c634355
chericc <1461021702@qq.com>
2025-05-04 11:45:03 +0000

    new: rtsp client

---
+++ demos/rtsp/client/main.cpp
@@ -0,0 +1,18 @@
+#include "rtsp_client.h"
+
+#include "xlog.hpp"
+#include "rtsp_client.h"
+
+int main()
+{
+    xlog_dbg("hello\n");
+
+    struct rtsp_client_param param = {};
+    snprintf(param.username, sizeof(param.username), "admin");
+    snprintf(param.password, sizeof(param.password), "123456");
+    snprintf(param.url, sizeof(param.url), "%s", "rtsp://10.0.0.3:8888/test.mkv");
+
+    rtsp_client_start(&param);
+
+    return 0;
+}
