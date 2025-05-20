c419bca760712641f7920c6587092b3df95caaa9
chericc <1461021702@qq.com>
2025-05-20 19:52:31 +0800

    python: usb device

---
+++ python/usbtest/usb_device.py
@@ -0,0 +1,25 @@
+import wmi
+
+def list_usb_ports():
+    w = wmi.WMI()
+    pnps = w.Win32_PnPEntity()
+    for p in pnps:
+        # print(p.PNPDeviceID)
+        # print(p)
+        name = ''
+        name = p.PNPDeviceID
+        if 'VID_0BDA' in name.upper() and 'PID_5411' in name.upper():
+            print(f'{name}')
+    # usbcontrollers = w.Win32_USBController()
+    # for usb in usbcontrollers:
+    #     print(usb.PNPDeviceID)
+    # usbcontroller_dev = w.Win32_USBControllerDevice()
+    # for u_d in usbcontroller_dev:
+    #     print(f'{u_d.Antecedent.PNPDeviceID}\n --{u_d.Dependent.PNPDeviceID}')
+
+
+
+if __name__ == "__main__":
+    print("begin123")
+    list_usb_ports()
+    print("end123")
