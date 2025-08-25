42a16712f4ff63478a967626e72851de937e41b3
chericc <1461021702@qq.com>
2025-08-25 12:46:00 +0000

    logtailer update

--- demos/log_tailer/log_tailer.cpp
+++ demos/log_tailer/log_tailer.cpp
@@ -1,15 +1,20 @@
 #include "log_tailer.h"
 
-#include <stdio.h>
+#include <string>
+#include <cstdio>
 
 struct log_tailer_ctx {
+    
     FILE *fp;
 
 };
 
 log_tailer_handle log_tailer_open(const char *path, log_tailer_cb cb)
 {
-    
+    do {
+        
+    } while (0);
+
 }
 
 int log_tailer_run_process(log_tailer_handle handle)
--- demos/log_tailer/log_tailer.h
+++ demos/log_tailer/log_tailer.h
@@ -3,8 +3,17 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
 typedef void* log_tailer_handle;
 typedef void (*log_tailer_cb)(const char *line, size_t size);
 log_tailer_handle log_tailer_open(const char *path, log_tailer_cb cb);
 int log_tailer_run_process(log_tailer_handle handle);
 int log_tailer_close(log_tailer_handle handle);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
