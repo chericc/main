db4750b33668169b715207ac2d384123bc5af5fb
chericc <1461021702@qq.com>
2025-10-17 09:35:16 +0800

    fix(accessor): better name as lock

--- demos/accesser/accessor.cpp
+++ demos/accesser/accessor.cpp
@@ -13,13 +13,17 @@ private:
     std::shared_ptr<ctx> _ctx;
 
 public:
-    class Accessor {
+    class Lock {
     private:
         std::shared_ptr<ctx> _ctx;
         std::shared_ptr<std::lock_guard<std::mutex>> _lock;
     public:
-        Accessor(std::shared_ptr<ctx> ctx) : _ctx(ctx) {
+        Lock(std::shared_ptr<ctx> ctx) : _ctx(ctx) {
+            // printf("locking\n");
             _lock = std::make_shared<std::lock_guard<std::mutex>>(_ctx->mutex);
+        }
+        ~Lock() {
+            // printf("unlocking\n");
         }
         _Type & ref() const {
             return _ctx->data;
@@ -32,19 +36,35 @@ public:
     ControledData() {
         _ctx = std::make_shared<ctx>();
     }
-    Accessor genAccessor() const {
-        return Accessor(_ctx);
+    Lock genLock() const {
+        return Lock(_ctx);
     }
 };
 
 int main()
 {
     ControledData<std::vector<int>> data;
-    data.genAccessor().ref().push_back(1);
 
-    auto const &ref = data.genAccessor().cref();
-    for (auto const & ref_item : ref) {
-        printf("%d\n", ref_item);
+    {
+        auto lock = data.genLock();
+        printf("take ref\n");
+        lock.ref().push_back(1);    
+        printf("take ref end\n");
+    }
+    
+
+    {
+        auto lock = data.genLock();
+        printf("take ref\n");
+        lock.ref().push_back(2);
+        printf("take ref end\n");
+    }
+
+    {
+        auto lock = data.genLock();
+        for (auto const & ref_item : lock.cref()) {
+            printf("%d\n", ref_item);
+        }
     }
     return 0;
 }
