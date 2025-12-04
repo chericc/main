a03dccb230460c103829ef966ade1f590870b0a8
chericc <1461021702@qq.com>
2025-12-04 23:00:45 +0800

    feat(sqlpp11): update

--- demos/sqlpp11/main.cpp
+++ demos/sqlpp11/main.cpp
@@ -1,6 +1,25 @@
 #include "sqlpp11/sqlpp11.h"
+#include "sqlpp11/sqlite3/sqlite3.h"
+
+namespace sql = sqlpp::sqlite3;
 
 int main()
 {
+    sql::connection_config config;
+    config.path_to_database = "test.db";
+    config.flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+    config.debug = true;
+
+    sql::connection db(config);
+    db.execute(
+R"(
+create table student (
+id integer primary key,
+name text not null,
+gender text not null
+);
+)"
+    );
+
     return 0;
 }
