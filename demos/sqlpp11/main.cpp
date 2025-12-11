9411f7af88cfa84a5f88b65dc05b22a9f732a382
chericc <1461021702@qq.com>
2025-12-11 17:04:37 +0800

    feat(sqlpp11): better serialize demo

--- demos/sqlpp11/main.cpp
+++ demos/sqlpp11/main.cpp
@@ -20,39 +20,6 @@ sqlpp::value_or_null_t<Type> to_sql(std::string name)
     }
 }
 
-
-struct _serializer_context_t
-{
-    ::sqlpp::detail::float_safe_ostringstream _os;
-
-    _serializer_context_t() = default;
-    _serializer_context_t(const _serializer_context_t& rhs)
-    {
-        _os << rhs._os.str();
-    }
-
-    std::string str() const
-    {
-        return _os.str();
-    }
-
-    void reset()
-    {
-        _os.str("");
-    }
-
-    template <typename T>
-    ::sqlpp::detail::float_safe_ostringstream& operator<<(T t)
-    {
-        return _os << t;
-    }
-
-    static std::string escape(std::string arg)
-    {
-        return sqlpp::serializer_context_t::escape(arg);
-    }
-};
-
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
@@ -85,9 +52,9 @@ int main(int argc, char *argv[])
         xlog_dbg("name=%s, number=%s", ref.name.text, ref.number.text);
     }
 
-    _serializer_context_t seri;
-    auto str = sqlpp::serialize(select(stu.number), seri);
-    xlog_dbg("se: %s", str.str().c_str());
+    sqlpp::sqlite3::context_t ctx(db);
+    auto sqlStr = sqlpp::serialize(select(stu.number), ctx).str();
+    xlog_dbg("se: %s", sqlStr.c_str());
 
     // join
     for (auto const& row : db(select(stu.name, stu_cla_rel.studentId).
