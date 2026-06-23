bcf2959e7323bbdc69436cc4f13d51041088a5f7
chericc <1461021702@qq.com>
2026-06-23 21:42:22 +0800

    feat: add model compilation, validation training, and plotting for IMDB dataset (section 4.1.3-4.1.4)

--- cs/DeepLearningWithPython/src/c4_1_1_IMDB数据集.py
+++ cs/DeepLearningWithPython/src/c4_1_1_IMDB数据集.py
@@ -48,3 +48,58 @@ model = keras.Sequential([
     layers.Dense(1, activation="sigmoid")
 ])
 
+# 4.1.3 编译模型
+model.compile(optimizer="rmsprop",
+                loss="binary_crossentropy",
+                metrics=["accuracy"])
+
+
+# 4.1.4 留出验证集
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+print(f'x_val.len={len(x_val)}')
+print(f'partial_x_train={len(partial_x_train)}')
+'''
+x_val.len=10000
+partial_x_train=15000
+'''
+
+# 4.1.4 训练模型
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+print(f'history.history.keys(): {history.history.keys()}')
+'''
+history.history.keys(): dict_keys(['accuracy', 'loss', 'val_accuracy', 'val_loss'])
+'''
+
+# 绘制训练损失和验证损失
+import matplotlib.pyplot as plt
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, 'bo', label='training loss')
+plt.plot(epochs, val_loss_values, 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+# 绘制训练精度和验证精度
+plt.clf()
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+plt.plot(epochs, acc, 'bo', label='training acc')
+plt.plot(epochs, val_acc, 'b', label='validation acc')
+plt.title('traning and validation accuracy')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
